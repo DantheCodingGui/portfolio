51ef30ed98637e4a2a978168a0511cba671e0153
Dan <psydb3@nottingham.ac.uk>
2017-08-21 19:30:37 +0100

    Starting commit

---
+++ javascript/responsive.js
@@ -0,0 +1,9 @@
+function toggleResponsiveNavBar() {
+  var navBar = document.getElementById("nav_container");
+  if (navBar.className == "INSERT CLASS NAME HERE") {
+    navBar.className += " responsive";
+  }
+  else {
+    navBar.className = "INSERT CLASS NAME HERE";
+  }
+}

1d99adc41a4594b5cec1c4b74042f0007b6dc6ae
Dan <psydb3@nottingham.ac.uk>
2017-08-23 17:43:45 +0100

    Fully responsive subtitle header

--- javascript/responsive.js
+++ javascript/responsive.js
@@ -2,9 +2,11 @@ function toggleResponsiveNavBar() {
   var navBar = document.getElementById("nav_container");
   if (navBar.classList.contains("responsive")) {
     navBar.classList.remove("responsive");
+    document.getElementById("responsive_menu").innerHTML = "&#9776;";
   }
   else {
     navBar.className = "responsive";
     document.getElementById("header").style.height = "auto";
+    document.getElementById("responsive_menu").innerHTML = "&#x2715";
   }
 }
